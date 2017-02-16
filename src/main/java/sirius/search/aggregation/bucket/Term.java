d2b2479fb13f674e7796474d1fe11245080806ff
Tobias Bagg <tba@scireum.de>
2017-02-16 16:31:21 +0100

    Ordering of term aggregations

--- src/main/java/sirius/search/aggregation/bucket/Term.java
+++ src/main/java/sirius/search/aggregation/bucket/Term.java
@@ -9,12 +9,16 @@
 package sirius.search.aggregation.bucket;
 
 import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
 import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
+import sirius.kernel.commons.Strings;
 
 /**
  * Represents an aggregation using terms
  */
 public class Term extends BucketAggregation {
+
+    private Boolean orderAsc = null;
 
     private Term(String name) {
         super(name);
@@ -34,12 +38,38 @@ public class Term extends BucketAggregation {
     }
 
     /**
+     * Sets ordering by {@link #field} to ascending.
+     *
+     * @return the term aggregation helper itself for fluent method calls
+     */
+    public Term orderAsc() {
+        orderAsc = true;
+        return this;
+    }
+
+    /**
+     * Sets ordering by {@link #field} to descending.
+     *
+     * @return the term aggregation helper itself for fluent method calls
+     */
+    public Term orderDesc() {
+        orderAsc = false;
+        return this;
+    }
+
+    /**
      * Constructs the termbuilder
      *
      * @return the termbuilder
      */
     @Override
     public TermsAggregationBuilder getBuilder() {
-        return AggregationBuilders.terms(getName()).field(getField()).size(getSize());
+        TermsAggregationBuilder builder = AggregationBuilders.terms(getName()).field(getField()).size(getSize());
+
+        if (orderAsc != null) {
+            builder.order(Terms.Order.term(orderAsc));
+        }
+
+        return builder;
     }
 }
