7905011587146ad29ee7de698e1f7659ef66753c
Dennis Heller <dhe@scireum.de>
2017-06-30 12:18:59 +0200

    Shows clearer message instead of NPE if no Entity was found for given type

--- src/main/java/sirius/search/IndexAccess.java
+++ src/main/java/sirius/search/IndexAccess.java
@@ -231,6 +231,9 @@ public class IndexAccess {
         try {
             String type = obj.getString("_type");
             Class<? extends Entity> entityClass = getType(type);
+            if (entityClass == null) {
+                throw new IllegalArgumentException("No Entity found with type \"" + type + "\"");
+            }
             EntityDescriptor descriptor = getDescriptor(entityClass);
             Entity entity = entityClass.newInstance();
             entity.setId(obj.getString("_id"));
