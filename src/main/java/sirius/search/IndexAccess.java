fe5deda4c93f350f4fb2f7be45c5625df82a66fb
Dennis Heller <dhe@scireum.de>
2017-06-30 12:17:58 +0200

    Fixes ES errors caused by loadDataset

--- src/main/java/sirius/search/IndexAccess.java
+++ src/main/java/sirius/search/IndexAccess.java
@@ -235,7 +235,7 @@ public class IndexAccess {
             Entity entity = entityClass.newInstance();
             entity.setId(obj.getString("_id"));
             descriptor.readSource(entity, obj);
-            update(entity);
+            create(entity);
         } catch (Exception e) {
             throw new IllegalArgumentException("Cannot load: " + obj, e);
         }
