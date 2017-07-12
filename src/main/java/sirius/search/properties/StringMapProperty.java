c7a6510b79c86055c7f6ab055987430a8d0833d7
Dennis Heller <dhe@scireum.de>
2017-07-12 14:22:04 +0200

    Do not store Maps as-is in elasticsearch anymore which prevents mapping explosion

--- src/main/java/sirius/search/properties/StringMapProperty.java
+++ src/main/java/sirius/search/properties/StringMapProperty.java
@@ -9,6 +9,7 @@
 package sirius.search.properties;
 
 import org.elasticsearch.common.xcontent.XContentBuilder;
+import sirius.kernel.commons.Context;
 import sirius.kernel.di.std.Register;
 import sirius.kernel.health.Exceptions;
 import sirius.search.Entity;
@@ -18,15 +19,22 @@ import sirius.web.http.WebContext;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import static sirius.search.properties.ESOption.FALSE;
 
 /**
  * Represents a property which contains a map of strings to strings. Such fields must wear a {@link
- * sirius.search.annotations.ListType} annotation with
- * <tt>String</tt> as their value.
+ * sirius.search.annotations.ListType} annotation with <tt>String</tt> as their value.
  */
-public class StringMapProperty extends Property {
+public class StringMapProperty extends StringProperty {
+
+    public static final String KEY = "key";
+    public static final String VALUE = "value";
 
     /**
      * Factory for generating properties based on their field type
@@ -52,15 +60,73 @@ public class StringMapProperty extends Property {
      */
     private StringMapProperty(Field field) {
         super(field);
+        setNested(true);
     }
 
     @Override
+    public void init(Entity entity) throws IllegalAccessException {
+        field.set(entity, new HashMap<>());
+    }
+
+    @Override
+    public boolean acceptsSetter() {
+        return false;
+    }
+
+    @Override
+    protected ESOption isDefaultIncludeInAll() {
+        return FALSE;
+    }
+
+    @Override
+    protected String getMappingType() {
+        return "nested";
+    }
+
+    /**
+     * Generates the mapping used by this property
+     *
+     * @param builder the builder used to generate JSON
+     * @throws IOException in case of an io error while generating the mapping
+     */
+    @Override
+    public void addMappingProperties(XContentBuilder builder) throws IOException {
+        super.addMappingProperties(builder);
+
+        builder.startObject("properties");
+
+        builder.startObject(KEY);
+        builder.field("type", "keyword");
+        builder.endObject();
+
+        builder.startObject(VALUE);
+        builder.field("type", super.getMappingType());
+        builder.endObject();
+
+        builder.endObject();
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
     protected Object transformFromSource(Object value) {
-        if (!(value instanceof Map)) {
-            return new HashMap<>();
-        } else {
-            return value;
+        Map<String, String> result = new HashMap<>();
+        if (value instanceof Collection) {
+            ((Collection<Map<String, String>>) value).forEach(entry -> result.put(entry.get(KEY), entry.get(VALUE)));
         }
+        return result;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    protected Object transformToSource(Object o) {
+        List<Map<String, Object>> valueList = new ArrayList<>();
+
+        if (o instanceof Map) {
+            ((Map<String, String>) o).forEach((key, value) -> valueList.add(Context.create()
+                                                                                   .set(KEY, key)
+                                                                                   .set(VALUE, value)));
+        }
+        return valueList;
     }
 
     @Override
@@ -75,54 +141,5 @@ public class StringMapProperty extends Property {
     @Override
     protected Object transformFromRequest(String name, WebContext ctx) {
         throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void init(Entity entity) throws IllegalAccessException {
-        field.set(entity, new HashMap<>());
-    }
-
-    @Override
-    protected String getMappingType() {
-        return "object";
-    }
-
-    @Override
-    public boolean acceptsSetter() {
-        return false;
-    }
-
-    /**
-     * Generates the mapping used by this property
-     *
-     * @param builder the builder used to generate JSON
-     * @throws IOException in case of an io error while generating the mapping
-     */
-    @Override
-    public void addMappingProperties(XContentBuilder builder) throws IOException {
-        builder.startObject(getName());
-        builder.field("type", getMappingType());
-        builder.endObject();
-    }
-
-    @Override
-    public void createDynamicTemplates(XContentBuilder builder) throws IOException {
-        builder.startObject();
-        {
-            builder.startObject(getName() + "_strings");
-            {
-                builder.field("path_match", getName() + ".*");
-                builder.field("match_mapping_type", "string");
-                builder.startObject("mapping");
-                {
-                    builder.field("type", "keyword");
-                    builder.field("store", "no");
-                    builder.field("include_in_all", false);
-                }
-                builder.endObject();
-            }
-            builder.endObject();
-        }
-        builder.endObject();
     }
 }
