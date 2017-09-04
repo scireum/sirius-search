30e423cf877d6e398176c10fe83c557297d714ef
Sascha Bieberstein <sbi@scireum.de>
2017-09-04 14:53:15 +0200

    Restores the ordering DateRanges and applies ot to the resulting facet

--- src/main/java/sirius/search/ResultList.java
+++ src/main/java/sirius/search/ResultList.java
@@ -16,8 +16,10 @@ import sirius.kernel.commons.Monoflop;
 import sirius.web.controller.Facet;
 
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * Combines the result items of a query along with the collected facet filters and aggregations.
@@ -37,8 +39,8 @@ public class ResultList<T> implements Iterable<T> {
     /**
      * Creates a new result list
      *
-     * @param termFacets   list of facets created by the query
-     * @param response     underlying search response building the result
+     * @param termFacets list of facets created by the query
+     * @param response   underlying search response building the result
      */
     protected ResultList(List<Facet> termFacets, SearchResponse response) {
         this.termFacets = termFacets;
@@ -105,15 +107,24 @@ public class ResultList<T> implements Iterable<T> {
         if (facetsProcessed.firstCall() && response != null) {
             for (Facet facet : termFacets) {
                 if (facet instanceof DateFacet) {
+                    DateFacet dateFacet = (DateFacet) facet;
+
+                    Map<String, Range.Bucket> items = new LinkedHashMap<>();
+                    dateFacet.getRanges().forEach(range -> items.put(range.getKey(), null));
+
                     Range range = response.getAggregations().get(facet.getName());
-                    for (Range.Bucket bucket : range.getBuckets()) {
-                        if (bucket.getDocCount() > 0) {
-                            DateRange dateRange = ((DateFacet) facet).getRangeByName(bucket.getKeyAsString());
-                            if (dateRange != null) {
-                                facet.addItem(dateRange.getKey(), dateRange.getName(), (int)bucket.getDocCount());
-                            }
+
+                    range.getBuckets().stream().filter(bucket -> bucket.getDocCount() > 0).forEach(bucket -> {
+                        DateRange dateRange = dateFacet.getRangeByName(bucket.getKeyAsString());
+                        if (dateRange != null) {
+                            items.put(dateRange.getKey(), bucket);
                         }
-                    }
+                    });
+
+                    items.values().stream().filter(Objects::nonNull).forEach(bucket -> {
+                        DateRange dateRange = dateFacet.getRangeByName(bucket.getKeyAsString());
+                        facet.addItem(dateRange.getKey(), dateRange.getName(), (int)bucket.getDocCount());
+                    });
                 } else {
                     Terms terms = response.getAggregations().get(facet.getName());
                     for (Terms.Bucket bucket : terms.getBuckets()) {
