f42b0ae243a28fb54fc1881c67f69da9a74a77a8
meg <meg@scireum.de>
2018-11-15 15:14:39 +0100

    Adds a method to indexAccess to override without running save handlers

--- src/main/java/sirius/search/IndexAccess.java
+++ src/main/java/sirius/search/IndexAccess.java
@@ -634,7 +634,8 @@ public class IndexAccess {
     public <E extends Entity> Optional<E> fetchOptionalFromCache(@Nullable String routing,
                                                                  @Nonnull Class<E> type,
                                                                  @Nullable String id,
-                                                                 @Nonnull com.google.common.cache.Cache<String, Object> cache) {
+                                                                 @Nonnull
+                                                                         com.google.common.cache.Cache<String, Object> cache) {
         return Optional.ofNullable(fetch(routing, type, id, cache).getFirst());
     }
 
@@ -1005,6 +1006,25 @@ public class IndexAccess {
     public <E extends Entity> E override(E entity) {
         try {
             return update(entity, false, false, true);
+        } catch (OptimisticLockException e) {
+            // Should never happen as version checking is disabled....
+            throw Exceptions.handle(LOG, e);
+        }
+    }
+
+    /**
+     * Updates the entity in the database without running the entities save checks and handlers.
+     * <p>
+     * As change tracking is disabled, this operation will override all previous changes which are not reflected
+     * by the entity to be saved.
+     *
+     * @param entity the entity to be written into the DB
+     * @param <E>    the type of the entity to override
+     * @return the updated entity
+     */
+    public <E extends Entity> E overrideUnchecked(E entity) {
+        try {
+            return update(entity, false, false, false);
         } catch (OptimisticLockException e) {
             // Should never happen as version checking is disabled....
             throw Exceptions.handle(LOG, e);
